b30f7b7371d32a34ec1d338cc36f92dc13533db9
Javier Pilco <brandon.pilco3401@utc.edu.ec>
2025-06-08 18:43:57 -0500

    Subida del proyecto 5.0

--- Aplicaciones/Empresas/urls.py
+++ Aplicaciones/Empresas/urls.py
@@ -9,11 +9,4 @@ urlpatterns = [
     path('editarCargo/<int:id>/', views.editarCargo),
     path('procesarEdicionCargo/', views.procesarEdicionCargo),
 
-    path('listarMensajes/', views.listarMensajes),
-    path('nuevoMensaje/', views.nuevoMensaje),
-    path('guardarMensaje/', views.guardarMensaje),
-    path('eliminarMensaje/<int:id>/', views.eliminarMensaje),
-    path('editarMensaje/<int:id>/', views.editarMensaje),
-    path('procesarEdicionMensaje/', views.procesarEdicionMensaje),
-    path('enviarMensaje/<int:id>/', views.enviarMensaje),
 ]
--- Aplicaciones/Empresas/views.py
+++ Aplicaciones/Empresas/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render,redirect, get_object_or_404 # type: ignore
+from django.shortcuts import render,redirect
 #importando el modelo cargo
-from .models import Cargo, Mensaje
+from .models import Cargo
 from django.contrib import messages # type: ignore
 import os
 from django.conf import settings
-import mimetypes
-from django.core.mail import EmailMessage
+
 
 
 # Create your views here.
@@ -81,92 +80,4 @@ def procesarEdicionCargo(request):
     # Mensaje de confirmación
     messages.success(request, "Cargo ACTUALIZADO exitosamente")
     return redirect('/')
-
-###MENSAJE###
-
-# Mostrar listado de mensajes
-def listarMensajes(request):
-    mensajes = Mensaje.objects.all()
-    return render(request, "listarMensajes.html", {'mensajes': mensajes})
-
-# Mostrar formulario de nuevo mensaje
-def nuevoMensaje(request):
-    return render(request, "nuevoMensaje.html")
-
-# Guardar nuevo mensaje
-def guardarMensaje(request):
-    destinatario = request.POST["destinatario"]
-    asunto = request.POST["asunto"]
-    mensaje_txt = request.POST["mensaje"]
-    archivo = request.FILES.get("archivo")
-
-    Mensaje.objects.create(
-        destinatario=destinatario,
-        asunto=asunto,
-        mensaje=mensaje_txt,
-        archivo=archivo
-    )
-
-    messages.success(request, "Mensaje GUARDADO exitosamente")
-    return redirect('/listarMensajes')
-
-# Eliminar mensaje
-def eliminarMensaje(request, id):
-    mensaje = Mensaje.objects.get(id=id)
-    
-    # Eliminar archivo adjunto si existe
-    if mensaje.archivo and os.path.isfile(mensaje.archivo.path):
-        os.remove(mensaje.archivo.path)
-    
-    mensaje.delete()
-    messages.success(request, "Mensaje ELIMINADO exitosamente")
-    return redirect('/listarMensajes')
-
-# Mostrar formulario de edición
-def editarMensaje(request, id):
-    mensajeEditar = Mensaje.objects.get(id=id)
-    return render(request, "editarMensaje.html", {'mensajeEditar': mensajeEditar})
-
-# Procesar edición de mensaje
-def procesarEdicionMensaje(request):
-    id = request.POST["id"]
-    destinatario = request.POST["destinatario"]
-    asunto = request.POST["asunto"]
-    mensaje_txt = request.POST["mensaje"]
-    archivo = request.FILES.get("archivo")
-
-    mensaje = Mensaje.objects.get(id=id)
-    mensaje.destinatario = destinatario
-    mensaje.asunto = asunto
-    mensaje.mensaje = mensaje_txt
-
-    if archivo:
-        if mensaje.archivo and os.path.isfile(mensaje.archivo.path):
-            os.remove(mensaje.archivo.path)
-        mensaje.archivo = archivo
-
-    mensaje.save()
-    messages.success(request, "Mensaje ACTUALIZADO exitosamente")
-    return redirect('/listarMensajes')
-
-# Enviar mensaje por correo
-def enviarMensaje(request, id):
-    mensaje = get_object_or_404(Mensaje, id=id)
-
-    email = EmailMessage(
-        subject=mensaje.asunto,
-        body=mensaje.mensaje,
-        from_email=settings.EMAIL_HOST_USER,
-        to=[mensaje.destinatario],
-    )
-
-    if mensaje.archivo:
-        file_type, _ = mimetypes.guess_type(mensaje.archivo.name)
-        with open(mensaje.archivo.path, 'rb') as f:
-            email.attach(mensaje.archivo.name, f.read(), file_type)
-
-    email.send(fail_silently=False)
-    messages.success(request, "Mensaje ENVIADO exitosamente")
-    return redirect('/listarMensajes')
-
 
